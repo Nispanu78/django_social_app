b3a9fc9cbceb6213f6ce4e12a40d5d509bfa0a74
Nicola <nicspanu@gmail.com>
2021-08-05 10:28:41 +0200

    dashboard

--- env/bookmarks/account/views.py
+++ env/bookmarks/account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 # from django.contrib import messages
 # from .models import Profile
 from .forms import LoginForm
@@ -27,3 +27,9 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
