25504abca4fdb171db738a8ed46b0627431d2a6e
Nicola <nicspanu@gmail.com>
2021-08-06 15:23:05 +0200

    authentication

--- env/bookmarks/settings.py
+++ env/bookmarks/settings.py
@@ -135,7 +135,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
-    'social_core.backends.facebook.FacebookOAuth2',
-#     'social_core.backends.twitter.TwitterOAuth',
-#     'social_core.backends.google.GoogleOAuth2',
 ]
