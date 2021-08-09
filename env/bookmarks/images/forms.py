c080cc6d58b9322e13791b5b16cca3ac9f99ce9f
Nicola <nicspanu@gmail.com>
2021-08-09 15:41:18 +0200

    javascript

--- env/bookmarks/images/forms.py
+++ env/bookmarks/images/forms.py
@@ -13,16 +13,16 @@ class ImageCreateForm(forms.ModelForm):
             'url': forms.HiddenInput,
         }
 
-        def clean_url(self):
-            url = self.cleaned_data['url']
-            valid_extensions = ['jpg', 'jpeg']
-            extension = url.rsplit('.', 1)[1].lower()
-            if extension not in valid_extensions:
-                raise forms.ValidationError('The given URL does not ' \
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not ' \
                                         'match valid image extensions.')
-            return url
+        return url
 
-        def save(self, force_insert=False,
+    def save(self, force_insert=False,
                    force_update=False,
                    commit=True):
         image = super().save(commit=False)
