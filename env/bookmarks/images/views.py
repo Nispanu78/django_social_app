abca50cda310cd6ec0d80e80750822025a67be13
Nicola <nicspanu@gmail.com>
2021-08-09 14:57:41 +0200

    views and urls

--- env/bookmarks/images/views.py
+++ env/bookmarks/images/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+# from django.http import JsonResponse
+# from django.views.decorators.http import require_POST
+# from django.http import HttpResponse
+# from django.core.paginator import Paginator, EmptyPage, \
+#                                   PageNotAnInteger
+# from common.decorators import ajax_required
+from .forms import ImageCreateForm
+# from .models import Image
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+
+            # assign current user to the item
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+
+            # redirect to new created item detail view
+            return redirect(new_item.get_absolute_url())
+    else:
+        # build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
