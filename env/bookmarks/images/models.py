f94a47dc03a2e778a162fcd5bd2e8fcead219f60
Nicola <nicspanu@gmail.com>
2021-08-09 13:32:25 +0200

    many to many

--- env/bookmarks/images/models.py
+++ env/bookmarks/images/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-# from django.utils.text import slugify
+from django.utils.text import slugify
 # from django.urls import reverse
 
 
@@ -16,17 +16,17 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True,
                                db_index=True)
-    # users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
-    #                                     related_name='images_liked',
-    #                                     blank=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     def __str__(self):
         return self.title
 
-    # def save(self, *args, **kwargs):
-    #     if not self.slug:
-    #         self.slug = slugify(self.title)
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
     #
     # def get_absolute_url(self):
     #     return reverse('images:detail', args=[self.id, self.slug])
