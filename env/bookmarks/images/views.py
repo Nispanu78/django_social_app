9bb6b0c0f20e9bc2bcf13ba89eadd63e3ccb022d
Nicola <nicspanu@gmail.com>
2021-08-10 15:38:10 +0200

    ajax

--- env/bookmarks/images/views.py
+++ env/bookmarks/images/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-# from django.http import JsonResponse
-# from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 # from django.http import HttpResponse
 # from django.core.paginator import Paginator, EmptyPage, \
 #                                   PageNotAnInteger
@@ -44,3 +44,20 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except:
+            pass
+    return JsonResponse({'status':'error'})
