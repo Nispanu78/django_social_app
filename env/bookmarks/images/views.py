202b135a9dc65dbcdf981877ea38c30b0e95292f
Nicola <nicspanu@gmail.com>
2021-08-11 11:59:15 +0200

    ajax

--- env/bookmarks/images/views.py
+++ env/bookmarks/images/views.py
@@ -4,10 +4,10 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-# from django.http import HttpResponse
-# from django.core.paginator import Paginator, EmptyPage, \
-#                                   PageNotAnInteger
-# from common.decorators import ajax_required
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -45,6 +45,7 @@ def image_detail(request, id, slug):
                   {'section': 'images',
                    'image': image})
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
