45f5205b556e99131ba2847378ed63554f2913f0
Nicola <nicspanu@gmail.com>
2021-08-16 14:39:53 +0200

    views

--- env/bookmarks/account/views.py
+++ env/bookmarks/account/views.py
@@ -88,3 +88,13 @@ def user_list(request):
                   'account/user/list.html',
                   {'section': 'people',
                    'users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,
+                             username=username,
+                             is_active=True)
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
