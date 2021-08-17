eeba63cefaf28b8f4b18592e709eee0572393c02
Nicola <nicspanu@gmail.com>
2021-08-17 16:03:08 +0200

    views

--- env/bookmarks/account/views.py
+++ env/bookmarks/account/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from actions.utils import create_action
+from actions.models import Action
 from .models import Contact
 
 
@@ -36,9 +37,20 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 def register(request):
     if request.method == 'POST':
